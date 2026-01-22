996790ae2c01dae9bdf188a45d8ad1d2b51326ae
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2026-01-22 12:08:08 +0530

    Coded to print OOPS to Console

--- src/main/java/com/bridgelabz/bannerapp/PrintOOPSBanner.java
+++ src/main/java/com/bridgelabz/bannerapp/PrintOOPSBanner.java
@@ -1,5 +1,13 @@
 package com.bridgelabz.bannerapp;
 
+/**
+ * OOPSBannerAPP UC1- OOPS Banner Display Application
+ * This class prints the banner "OOPS" to the console.
+ *
+ * @author Mohammed
+ * @version 1.0
+ */
+
 public class PrintOOPSBanner {
     public static void main(String[] args) {
         System.out.println("OOPS");
